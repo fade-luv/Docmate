d7ccbc314b630a43df33791648316d2c3cf017a3
Бойко Сергей <sergey.boyko@ecomdoc.ru@fox-00000101.doc.ipa>
2023-07-03 09:49:34 +0300

    create server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+http.createServer(function(request, response){
+    console.log ("Url: " + request.url);
+    console.log ("Тип запроса:" + request.method)
+    console.log ("User Agent: " + request.headers["user-agent"])
+    console.log ("Все заголовки")
+    console.log (request.headers)
+    response.end("Hello world!");
+}).listen (3000);
